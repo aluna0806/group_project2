6231862862615377739b4248c0da266ace7ce0b2
aluna0806 <63885788+aluna0806@users.noreply.github.com>
2020-08-03 17:55:09 -0600

    sql tables update

--- tablebuild_manual.sql
+++ tablebuild_manual.sql
@@ -1,44 +1,22 @@
-select * from wnba_Teams
-
-CREATE TABLE wnba_Teams (
-	season int	NOT NULL,
-	team_name varchar(10)	NOT NULL,
-	city varchar(50)	NOT NULL,
-	coach varchar(50)	NOT NULL,
-    PRIMARY KEY (season, team_name)
-);
-
-CREATE TABLE Team_Roster (
-	season int	NOT NULL,
-	team_name varchar(50)	NOT NULL,
-	first_name varchar(50)	NOT NULL,
-	last_name varchar(50)	NOT NULL,
-	PRIMARY KEY (season, team_name)
-); 
-
-CREATE TABLE Game_Stats (
+CREATE TABLE Game_Stats_2018 (
     season int	NOT NULL,
+	"date" DATE	NOT NULL,
 	home_team varchar(10)	NOT NULL,
 	away_team varchar(10)	NOT NULL,
 	home_score varchar(10)	NOT NULL,
-	away_score varchar(10)	NOT NULL, 
-    PRIMARY KEY (season),
-	FOREIGN KEY (home_team) REFERENCES wnba_Team(team_name),
-	FOREIGN KEY (away_team) REFERENCES wnba_Team(team_name)
+	away_score varchar(10)	NOT NULL
 );
 
-CREATE TABLE Season_Highlights (
+CREATE TABLE Game_Stats_2019 (
     season int	NOT NULL,
+	"date" DATE	NOT NULL,
 	home_team varchar(10)	NOT NULL,
 	away_team varchar(10)	NOT NULL,
 	home_score varchar(10)	NOT NULL,
-	away_score varchar(10)	NOT NULL, 
-    PRIMARY KEY (season),
-	FOREIGN KEY (home_team) REFERENCES WNBA_Team(team_name),
-	FOREIGN KEY (away_team) REFERENCES WNBA_Team(team_name)
+	away_score varchar(10)	NOT NULL 
 );
 
-CREATE TABLE Player_Profile (
+CREATE TABLE Player_Profile_2018 (
     season int   NOT NULL,
     first_name varchar(50)	NOT NULL,
     last_name varchar(50)	NOT NULL,
@@ -49,14 +27,20 @@ CREATE TABLE Player_Profile (
 	fieldgoal_percentage int	NOT NULL,
 	freethrows int	NOT NULL,
 	freethrow_percentage int	NOT NULL,
-	overall_points int	NOT NULL,
-	PRIMARY KEY (season,last_name),
-	FOREIGN KEY (team_name) REFERENCES Team_Roster(team_name)
+	overall_points int	NOT NULL
 );
 
-CREATE TABLE Season_Highlights (
-    season int	NOT NULL,
-    champion varchar(50)	NOT NULL,
-	mvp_playerlastnname varchar(50)	NOT NULL,
-	PRIMARY KEY (mvp_playerlastname)
+CREATE TABLE Player_Profile_2019 (
+    season int   NOT NULL,
+    first_name varchar(50)	NOT NULL,
+    last_name varchar(50)	NOT NULL,
+    team_name varchar(50)	NOT NULL,
+    games_played int	NOT NULL,
+    minutes_played int	NOT NULL,
+    fieldgoals int	NOT NULL,
+	fieldgoal_percentage int	NOT NULL,
+	freethrows int	NOT NULL,
+	freethrow_percentage int	NOT NULL,
+	overall_points int	NOT NULL
 );
+
